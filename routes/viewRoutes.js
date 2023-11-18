2531565ccc07aa0c8de458eb11dfea847a0cacde
Diaa yousef <105864744+Dyousef5@users.noreply.github.com>
2023-11-18 12:47:35 +0200

    add

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const viewsController = require('./../controllers/viewsController')
+const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.use(viewsController.alerts);
 
-router.route('/').get(viewsController.getOverview)
-router.route('/tour/:slug').get(viewsController.getTour);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
-module.exports = router
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
+
+router.post(
+  '/submit-user-data',
+  authController.protect,
+  viewsController.updateUserData
+);
+
+module.exports = router;
